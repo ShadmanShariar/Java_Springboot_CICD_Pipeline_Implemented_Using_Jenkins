06a75e6033cdce8253a0059528561c5c1a767e1b
Shadman Shariar <shadmanshariar@Shadmans-MacBook-Pro.local>
2025-01-07 03:05:39 +0600

    first commit

---
+++ src/main/java/com/example/jenkins/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/message")
+    public String greet(){
+        return "CICD using Jenkins for test";
+    }
+
+
+}
